8bd8827a515bef1a1700ba3de7596ff9e986dc13
Jonathan Gruber <gruberjonathan@gmail.com>
2023-07-17 09:26:14 +0200

    Close server on SIGTERM

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import app from './index';
 
-app.listen(process.env.PORT ?? 8080, () =>
+const server = app.listen(process.env.PORT ?? 8080, () =>
   console.log(
     `Server listening on http://localhost:${process.env.PORT || 8080}`,
   ),
 );
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('SIGTERM - HTTP server closed');
+  });
+});
