6ad6dc8c1d6c8aa65fbf8bf4b4bdde4767c13b2a
Jonathan Gruber <gruberjonathan@gmail.com>
2023-07-17 11:40:41 +0200

    Fix open handles in Jest that prevent test completion
    In Node 18, the native fetch leads to an open handle in Jest preventing it from terminating: https://github.com/nodejs/help/issues/3903.
    
    Another problem seems memory-cache. If the cache has entries, Jest will also run into the open handle issue.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,6 @@
 import request from 'supertest';
+import { Server } from 'http';
+import cache from 'memory-cache';
 
 import app from '../src';
 import * as fetchService from '../src/fetch';
@@ -7,82 +9,99 @@ import testData from './fixtures/grubersjoe-2018.json';
 import testDataMultipleYears from './fixtures/grubersjoe-2017-2018.json';
 import testDataNested from './fixtures/grubersjoe-2018-nested.json';
 
-const version = 4;
+const version = 'v4';
 const username = 'grubersjoe';
 
 describe('The :username endpoint', () => {
-  test('should return correct data for specific year', async () => {
-    const response = await request(app).get(`/v${version}/${username}?y=2018`);
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toStrictEqual(testData);
+  let server: Server;
+
+  beforeEach((done) => {
+    server = app.listen(done);
   });
 
-  test('should return correct data for multiple years', async () => {
-    const response = await request(app).get(
-      `/v${version}/${username}?y=2017&y=2018`,
-    );
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toStrictEqual(testDataMultipleYears);
+  afterEach((done) => {
+    cache.clear();
+    server.close(done);
   });
 
-  test('should return correct data for last year', async () => {
-    const response = await request(app).get(`/v${version}/${username}?y=last`);
-    expect(response.statusCode).toBe(200);
-    expect(Object.keys(response.body.total)).toContain('lastYear');
-    expect(typeof response.body.total.lastYear).toBe('number');
-  });
+  test('should return correct data for specific year', () =>
+    request(app)
+      .get(`/${version}/${username}?y=2018`)
+      .expect(200)
+      .expect(({ body }) => expect(body).toStrictEqual(testData)));
 
-  test('should return correct data for specific year in nested format', async () => {
-    const response = await request(app).get(
-      `/v${version}/${username}?y=2018&format=nested`,
-    );
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toStrictEqual(testDataNested);
-  });
+  test('should return correct data for multiple years', async () =>
+    request(app)
+      .get(`/${version}/${username}?y=2017&y=2018`)
+      .expect(200)
+      .expect((res) => expect(res.body).toStrictEqual(testDataMultipleYears)));
 
-  test('should return empty response if no data is available', async () => {
-    const response = await request(app).get(`/v${version}/${username}?y=1900`);
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toStrictEqual({
-      total: {},
-      contributions: [],
-    });
-  });
+  test('should return correct data for last year', () =>
+    request(app)
+      .get(`/${version}/${username}?y=last`)
+      .expect(200)
+      .expect(({ body }) => {
+        expect(Object.keys(body.total)).toContain('lastYear');
+        expect(typeof body.total.lastYear).toBe('number');
+      }));
 
-  test('should respond 404 if user cannot be found', async () => {
-    const username = '9520460126';
-    const response = await request(app).get(`/v${version}/${username}`);
-    expect(response.statusCode).toBe(404);
-    expect(response.body).toStrictEqual({
-      error: `User \'${username}\' not found`,
-    });
+  test('should return correct data for specific year in nested format', () =>
+    request(app)
+      .get(`/${version}/${username}?y=2018&format=nested`)
+      .expect(200)
+      .expect(({ body }) => expect(body).toStrictEqual(testDataNested)));
+
+  test('should return empty response if no data is available', () =>
+    request(app)
+      .get(`/${version}/${username}?y=1900`)
+      .expect(200)
+      .expect(({ body }) =>
+        expect(body).toStrictEqual({
+          total: {},
+          contributions: [],
+        }),
+      ));
+
+  test('should respond 404 if user cannot be found', () => {
+    const nonExistingUser = '43b83cb5-2d8f-44d3-b01c-98a73af7a15f';
+
+    return request(app)
+      .get(`/${version}/${nonExistingUser}`)
+      .expect(404)
+      .expect(({ body }) =>
+        expect(body).toStrictEqual({
+          error: `User \'${nonExistingUser}\' not found`,
+        }),
+      );
   });
 
   test.each([[`y=invalid`], [`y=2020abc`], [`y=abc2020`], [`y=`]])(
     'should respond 400 for invalid y query parameter %s',
-    async y => {
-      const response = await request(app).get(`/v${version}/${username}?${y}`);
-      expect(response.statusCode).toBe(400);
-      expect(response.body).toStrictEqual({
-        error: "Query parameter 'y' must be an integer, 'all' or 'last'",
-      });
-    },
+    (y) =>
+      request(app)
+        .get(`/${version}/${username}?${y}`)
+        .expect(400)
+        .expect(({ body }) =>
+          expect(body).toStrictEqual({
+            error: "Query parameter 'y' must be an integer, 'all' or 'last'",
+          }),
+        ),
   );
 
   test.each([[`format=invalid`], [`format=`]])(
     'should respond 400 for invalid format query parameter %s',
-    async () => {
-      const response = await request(app).get(
-        `/v${version}/${username}?format=invalid`,
-      );
-      expect(response.statusCode).toBe(400);
-      expect(response.body).toStrictEqual({
-        error: "Query parameter 'format' must be 'nested' or undefined",
-      });
-    },
+    () =>
+      request(app)
+        .get(`/${version}/${username}?format=invalid`)
+        .expect(400)
+        .expect(({ body }) =>
+          expect(body).toStrictEqual({
+            error: "Query parameter 'format' must be 'nested' or undefined",
+          }),
+        ),
   );
 
-  test('should respond 500 if an error is thrown', async () => {
+  test('should respond 500 if an error is thrown', () => {
     const fetchContributionsMock = jest.spyOn(
       fetchService,
       'fetchContributionsForQuery',
@@ -92,11 +111,13 @@ describe('The :username endpoint', () => {
       throw new Error('unexpected error');
     });
 
-    const response = await request(app).get(`/v${version}/${username}`);
-
-    expect(response.statusCode).toBe(500);
-    expect(response.body).toStrictEqual({
-      error: `Unable to fetch contribution data of '${username}': unexpected error.`,
-    });
+    return request(app)
+      .get(`/${version}/${username}`)
+      .expect(500)
+      .expect((res) =>
+        expect(res.body).toStrictEqual({
+          error: `Unable to fetch contribution data of '${username}': unexpected error.`,
+        }),
+      );
   });
 });
