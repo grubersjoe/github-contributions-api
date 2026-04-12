b9d4cca500ba5bbaeaa1d5894e78d60ae3c1e5d9
Jonathan Gruber <gruberjonathan@gmail.com>
2026-04-12 20:00:49 +0200

    Version bump and verbose tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    reporters: process.env.CI ? 'default' : 'verbose',
+    reporters: 'verbose',
     silent: true,
     testTimeout: 8000,
   },
