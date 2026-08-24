f90c2ff8e739dd8b0e01289557127811c31a6f8f
Jonathan Gruber <jonathan.gruber@gridfuse.com>
2026-08-24 14:27:49 +0200

    Add release to Sentry

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,8 +1,14 @@
 import * as Sentry from '@sentry/node'
+import { execFileSync } from 'node:child_process'
 import { log } from './app'
+
+const gitHash = execFileSync('git', ['rev-parse', '--short', 'HEAD'], {
+  encoding: 'utf8',
+}).trim()
 
 if (process.env.SENTRY_DSN) {
   Sentry.init({
+    release: gitHash,
     dsn: process.env.SENTRY_DSN,
     integrations: [Sentry.nodeRuntimeMetricsIntegration()],
     tracesSampleRate: 1.0,
@@ -10,7 +16,7 @@ if (process.env.SENTRY_DSN) {
       userInfo: true,
     },
   })
-  log(`Sentry initialized for DSN ${process.env.SENTRY_DSN}`)
+  log(`Sentry initialized`)
 } else {
   log(`Sentry disabled`)
 }
